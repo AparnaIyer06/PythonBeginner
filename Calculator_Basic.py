00534aa4abe942d11b7fcb82c9a9d96f4214f23f
AparnaIyer06 <111898420+AparnaIyer06@users.noreply.github.com>
2022-09-09 19:06:21 +0530

    Add files via upload
    A basic calculator that provides the addition, subtraction, multiplication and division options to the user.

---
+++ Calculator_Basic.py
@@ -0,0 +1,27 @@
+#Arithmetic Calculator
+'''
+num1=float(input("Enter a number: "))
+num2=float(input("Enter another number: "))
+
+user_choice=input("Choose one option for operation: \n1.Add \n2.Subtract \n3.Multiply \n4.Divide\n")
+if user_choice=='1':
+    soln=num1+num2
+    operation='Sum'
+elif user_choice=='2':
+    soln=num1-num2
+    operation='Difference'
+elif user_choice=='3':
+    soln=num1*num2
+    operation='Product'
+elif user_choice=='4':
+    soln=num1/num2
+    operation='Quotient'
+else:
+    print("Enter a valid choice of operation.")
+print("{0} is {1}.".format(operation,soln))
+print(operation,"is: %0.3f"%soln)
+'''
+print(7.2-6.3)
+print(bool())
+print(str(3.14))
+
